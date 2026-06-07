abe0e36a77b735e18cd1149b4a7c050025a40a14
Marcel Döinghaus <57318327+Kokosnuese@users.noreply.github.com>
2026-06-07 18:59:25 +0200

    Update tooltip.js

--- js/components/tooltip.js
+++ js/components/tooltip.js
@@ -1,4 +1,44 @@
-function showTooltip(
+function showTooltip(item){
+
+    const tooltip =
+        document.getElementById(
+            "tooltip"
+        );
+
+    tooltip.innerHTML = `
+
+        <div class="tooltip-name">
+
+            ${item.name}
+
+        </div>
+
+        <hr>
+
+        HP:
+        ${item.stats?.hp || 0}
+
+        <br>
+
+        Attack:
+        ${item.stats?.attack || 0}
+
+        <br>
+
+        Crit:
+        ${item.stats?.crit || 0}
+
+        <br>
+
+        Accuracy:
+        ${item.stats?.accuracy || 0}
+
+    `;
+
+    tooltip.style.display =
+        "block";
+
+}function showTooltip(
     item,
     x,
     y
