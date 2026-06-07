7054c71aa754c52bb44592865a4ccd39ce01c6a9
Marcel Döinghaus <57318327+Kokosnuese@users.noreply.github.com>
2026-06-07 17:12:40 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,33 @@
-console.log("Aion 4 Builder gestartet");
+async function init(){
+
+    const classes =
+        await loadClasses();
+
+    const selector =
+        document.getElementById(
+            "classSelector"
+        );
+
+    classes.forEach(c => {
+
+        const option =
+            document.createElement(
+                "option"
+            );
+
+        option.value = c.id;
+
+        option.textContent =
+            c.name;
+
+        selector.appendChild(
+            option
+        );
+
+    });
+
+    createEquipmentSlots();
+
+}
+
+init();console.log("Aion 4 Builder gestartet");
