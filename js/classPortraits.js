16ebe338988a7f28b1aa280aa8d1bdadf61132a3
Marcel Döinghaus <57318327+Kokosnuese@users.noreply.github.com>
2026-06-07 19:33:26 +0200

    Update classPortraits.js

--- js/classPortraits.js
+++ js/classPortraits.js
@@ -1,12 +1,32 @@
 const classPortraits = {
 
     gladiator:
-        "assets/ui/gladiator.png",
+        "assets/ui/icon_emblem_fighter.png",
 
     templar:
-        "assets/ui/templar.png",
+        "assets/ui/icon_emblem_knight.png",
 
     assassin:
-        "assets/ui/assassin.png"
+        "assets/ui/icon_emblem_assassin.png",
 
+    ranger:
+        "assets/ui/icon_emblem_ranger.png",
+
+    sorcerer:
+        "assets/ui/icon_emblem_wizard.png",
+
+    spiritmaster:
+        "assets/ui/icon_emblem_elementalist.png",
+
+    cleric:
+        "assets/ui/icon_emblem_wizard.png",
+
+    chanter:
+        "assets/ui/icon_emblem_chanter.png",
+
+    gunslinger:
+        "assets/ui/Icon_emblem_Gunner.png",
+
+    songweaver:
+        "assets/ui/Icon_emblem_Bard.png"
 };
