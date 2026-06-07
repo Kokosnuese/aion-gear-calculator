eb580c6b76a70700d93f05b958240add47b7a7da
Marcel Döinghaus <57318327+Kokosnuese@users.noreply.github.com>
2026-06-07 17:29:34 +0200

    Create equipmentManager.js

---
+++ js/equipmentManager.js
@@ -0,0 +1,31 @@
+const equipment = {
+
+    weapon:null,
+
+    subWeapon:null,
+
+    helmet:null,
+
+    shoulders:null,
+
+    chest:null,
+
+    gloves:null,
+
+    pants:null,
+
+    boots:null,
+
+    necklace:null,
+
+    earring1:null,
+
+    earring2:null,
+
+    ring1:null,
+
+    ring2:null,
+
+    belt:null
+
+};
