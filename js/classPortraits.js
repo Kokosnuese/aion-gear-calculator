9252278e0abaf78777691dd896d2b32ae0d1bc3d
Marcel Döinghaus <57318327+Kokosnuese@users.noreply.github.com>
2026-06-07 18:52:49 +0200

    Create classPortraits.js

---
+++ js/classPortraits.js
@@ -0,0 +1,12 @@
+const classPortraits = {
+
+    gladiator:
+        "assets/ui/gladiator.png",
+
+    templar:
+        "assets/ui/templar.png",
+
+    assassin:
+        "assets/ui/assassin.png"
+
+};
