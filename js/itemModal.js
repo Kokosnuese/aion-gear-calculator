bf3a8c24901f918f3c1a0e20499da60094b573c7
Marcel Döinghaus <57318327+Kokosnuese@users.noreply.github.com>
2026-06-07 17:52:41 +0200

    Update itemModal.js

--- js/itemModal.js
+++ js/itemModal.js
@@ -6,3 +6,33 @@ function openItemModal(slot){
     );
 
 }
+function filterModalItems(){
+
+    const search =
+        document
+        .getElementById(
+            "itemSearchInput"
+        )
+        .value
+        .toLowerCase();
+
+    const rows =
+        document.querySelectorAll(
+            ".item-row"
+        );
+
+    rows.forEach(row => {
+
+        row.style.display =
+
+            row.textContent
+            .toLowerCase()
+            .includes(search)
+
+            ? ""
+
+            : "none";
+
+    });
+
+}
