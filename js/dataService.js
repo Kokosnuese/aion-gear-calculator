49e8e94dec86eced4bd73fd51f4c2cf753c6b8d4
Marcel Döinghaus <57318327+Kokosnuese@users.noreply.github.com>
2026-06-07 19:28:03 +0200

    Update dataService.js

--- js/dataService.js
+++ js/dataService.js
@@ -26,3 +26,48 @@ function getArmorForClass(
     );
 
 }
+function getAccessoriesForSlot(slot){
+
+    switch(slot){
+
+        case "ring1":
+        case "ring2":
+
+            return accessories.filter(
+                item =>
+                    item.slot ===
+                    "ring"
+            );
+
+        case "earring1":
+        case "earring2":
+
+            return accessories.filter(
+                item =>
+                    item.slot ===
+                    "earring"
+            );
+
+        case "necklace":
+
+            return accessories.filter(
+                item =>
+                    item.slot ===
+                    "necklace"
+            );
+
+        case "belt":
+
+            return accessories.filter(
+                item =>
+                    item.slot ===
+                    "belt"
+            );
+
+        default:
+
+            return [];
+
+    }
+
+}
