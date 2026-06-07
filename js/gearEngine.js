535ba028f5c25f7e54b5167b51aacf68cfe98689
Marcel Döinghaus <57318327+Kokosnuese@users.noreply.github.com>
2026-06-07 19:11:29 +0200

    Update gearEngine.js

--- js/gearEngine.js
+++ js/gearEngine.js
@@ -37,37 +37,4 @@ function calculateStats(classId){
 
     return stats;
 
-}function calculateStats(base,equipment){
-
-    const stats = {
-
-        hp: base.hp,
-        attack: base.attack,
-        crit: base.crit,
-        accuracy: base.accuracy
-
-    };
-
-    Object.values(equipment).forEach(item => {
-
-        if(!item) return;
-
-        if(item.stats){
-
-            stats.hp += item.stats.hp || 0;
-
-            stats.attack +=
-                item.stats.attack || 0;
-
-            stats.crit +=
-                item.stats.crit || 0;
-
-            stats.accuracy +=
-                item.stats.accuracy || 0;
-        }
-
-    });
-
-    return stats;
-
 }
