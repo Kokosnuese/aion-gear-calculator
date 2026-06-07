c72ad06daf1156cb88ed36c8e389a4409ac84b17
Marcel Döinghaus <57318327+Kokosnuese@users.noreply.github.com>
2026-06-07 19:36:14 +0200

    Update equipmentSlot.js

--- js/components/equipmentSlot.js
+++ js/components/equipmentSlot.js
@@ -1,3 +1,47 @@
+
+const slotIcons = {
+
+    helmet:
+        "assets/ui/Helmet.png",
+
+    shoulders:
+        "assets/ui/Shoulder.png",
+
+    weapon:
+        "assets/ui/Weapon.png",
+
+    chest:
+        "assets/ui/Breast.png",
+
+    gloves:
+        "assets/ui/Glove.png",
+
+    pants:
+        "assets/ui/Pants.png",
+
+    boots:
+        "assets/ui/Shoes.png",
+
+    necklace:
+        "assets/ui/Necklaces.png",
+
+    earring1:
+        "assets/ui/EarringL.png",
+
+    earring2:
+        "assets/ui/EarringR.png",
+
+    ring1:
+        "assets/ui/RingL.png",
+
+    ring2:
+        "assets/ui/RingR.png",
+
+    belt:
+        "assets/ui/Belt.png"
+
+};
+
 function createSlot(slotName){
 
     const slot =
